27bfc161d3128a7136e6afb8dbfe20aa900736c6
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-16 17:30:20 +0100

    feat: add const grill on js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,7 @@
 //Costante per stabilire il livello di difficoltà
 const levelHtml = document.getElementById ('level')
 //Costante per attivare il gioco attraverso il bottone play
-const buttonHmtl = document.getElementById ('btn-play')
+const buttonHmtl = document.querySelector ('play')
+//Costante per attivare la griglia
+const grigliaHtml = document.querySelector('grill')
+
