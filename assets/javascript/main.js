e2b288374f3c5c52f234991ecb6feae38fddfe64
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-16 18:05:02 +0100

    feat: add main js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,49 @@
 //Costante per stabilire il livello di difficoltà
 const levelHtml = document.getElementById ('level')
 //Costante per attivare il gioco attraverso il bottone play
-const buttonHmtl = document.querySelector ('play')
+const buttonHmtl = document.querySelector ('.play')
 //Costante per attivare la griglia
-const grigliaHtml = document.querySelector('grill')
+const grigliaHtml = document.getElementById('grill')
 
+buttonHmtl.addEventListener('click', inizioGioco);
+
+function selectedSquareChangeColor(){
+    this.classList.add('box-selected-color');
+}
+
+function inizioGioco() {
+    
+    grigliaHtml.innerHTML = '';
+    if(level.value == 'easy') {
+        for (let i = 1; i <= 100; i++) {
+            let box = document.createElement("div");
+            box.classList.add("box-easy");
+            grigliaHtml.append(box);
+            box.innerHTML = i;
+
+            box.addEventListener('click', selectedSquareChangeColor);
+            console.log(i)
+        }
+
+    }else if (level.value == 'medium') {
+        for (let i = 1; i <= 81; i++) {
+            let box = document.createElement("div");
+            box.classList.add("box-medium");
+            grigliaHtml.append(box);
+            box.innerHTML = i;
+
+            box.addEventListener('click', selectedSquareChangeColor);
+            console.log(i)
+        }
+    }else if (level.value == 'hard') {
+        for (let i = 1; i <= 49; i++) {
+            let box = document.createElement("div");
+            box.classList.add("box-hard");
+            grigliaHtml.append(box);
+            box.innerHTML = i;
+
+            box.addEventListener('click', selectedSquareChangeColor);
+            console.log(i)
+        }
+    }
+}
