6689e29bc533047303936b81828e40ba10f26605
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-16 17:20:07 +0100

    feat: const JavaScript

---
+++ assets/javascript/main.js
@@ -0,0 +1,4 @@
+//Costante per stabilire il livello di difficoltà
+const levelHtml = document.getElementById ('level')
+//Costante per attivare il gioco attraverso il bottone play
+const buttonHmtl = document.getElementById ('btn-play')
